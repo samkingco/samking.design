056a4232c8776793a1dc2e1888a93f90c68423be
Sam King <mail@samking.co>
2022-05-16 19:02:06 +0100

    Fix Script being inside Head

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,15 @@
 import type { AppProps } from "next/app";
-import Head from "next/head";
 import Script from "next/script";
 import "../styles/globals.css";
 
 export default function CustomApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Head>
-        <Script
-          defer
-          data-domain="samking.design"
-          src="https://plausible.io/js/plausible.js"
-        />
-      </Head>
+      <Script
+        defer
+        data-domain="samking.design"
+        src="https://plausible.io/js/plausible.js"
+      />
       <Component {...pageProps} />
     </>
   );
